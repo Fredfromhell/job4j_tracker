43373fbd73b84a2de342687f42178d3b554b5fa2
FredfromHell <bespalov@avtocenter.ru>
2021-09-27 16:02:50 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,44 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+
+        Book history = new Book("History", 100);
+        Book roman = new Book("Roman", 200);
+        Book cleanCode = new Book("Clean code", 120);
+        Book fantastic = new Book("Fantastic", 140);
+        Book[] books = new Book[4];
+        books[0] = history;
+        books[1] = roman;
+        books[2] = cleanCode;
+        books[3] = fantastic;
+        System.out.println("First cycle" + System.lineSeparator());
+        for (Book array : books) {
+            System.out.println(array.getName() + " - " + array.getPages());
+        }
+
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+
+        System.out.println(System.lineSeparator() + "Second cycle" + System.lineSeparator());
+
+        for (Book array : books) {
+            System.out.println(array.getName() + " - " + array.getPages());
+
+        }
+
+        System.out.println(System.lineSeparator() + "Third cycle" + System.lineSeparator());
+
+        for (Book book : books) {
+            if (book.equals(cleanCode)) {
+                System.out.println(book.getName() + " - " + book.getPages());
+            }
+
+        }
+
+    }
+}
+
+
