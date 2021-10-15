7862e43bf1b5ac2e3795c7ec154e0178d5111447
FredfromHell <bespalov@avtocenter.ru>
2021-10-15 15:47:06 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #216380]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -17,21 +17,19 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (user.getUsername().length() < 3) {
+        if (user.getUsername().length() < 3 || !user.isValid()) {
             throw new UserInvalidException("Пользователь заблокирован.");
 
-        } else if (!user.isValid()) {
-            throw new UserInvalidException("Пользователь заблокирован.");
         }
             return true;
     }
 
     public static void main(String[] args) {
         User[] users = {
-                new User("Pe", true)
+                new User("Pet", false)
         };
         try {
-            User user = findUser(users, "Pe");
+            User user = findUser(users, "Pet");
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
