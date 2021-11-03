dcc0331121f003b44c1845339f209a85c26f5eef
FredfromHell <bespalov@avtocenter.ru>
2021-11-03 15:17:36 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094]

--- src/main/java/ru/job4j/collection/UsageAArrayList.java
+++ src/main/java/ru/job4j/collection/UsageAArrayList.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class UsageAArrayList {
 
     public static void main(String[] args) {
+
         ArrayList<String> names = new ArrayList<String>();
         names.add("Petr");
         names.add("Ivan");
