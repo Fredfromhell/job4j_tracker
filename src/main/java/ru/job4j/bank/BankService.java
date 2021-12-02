b94eda73df4ec05a83da965ce544aa0889b4ed5b
FredfromHell <bespalov@avtocenter.ru>
2021-12-02 18:21:35 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,74 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+
+    }
+
+    public void addAccount(String passport, Account account) {
+        User client = findByPassport(passport);
+        if (!users.get(client).contains(account)) {
+            users.get(client).add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        User rsl = null;
+        for (User key : users.keySet()) {
+            if (key.getPassport().equals(passport)) {
+                rsl = key;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account rsl = null;
+        User client = findByPassport(passport);
+        if (client != null) {
+            List<Account> accounts = users.get(client);
+            for (Account chek : accounts) {
+                if (chek.getRequisite().equals(requisite)) {
+                    rsl = chek;
+                    break;
+                }
+            }
+        }
+
+        return rsl;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && srcAccount.getBalance() >= amount) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            rsl = true;
+        }
+
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
+        bank.addUser(user);
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.getPassport(), new Account("113", 50D));
+        bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 20D);
+        System.out.println(bank.findByRequisite("3434", "113").getBalance());
+        System.out.println(bank.findByRequisite("3434", "5546").getBalance());
+    }
+}
