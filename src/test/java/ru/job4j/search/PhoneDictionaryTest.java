f3ca39f570940bab158f3ea9dc898242281531fe
Fredfromhell <bespalov@avtocnter.ru>
2022-02-09 09:40:54 +0300

    6. Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -3,10 +3,8 @@ package ru.job4j.search;
 import org.junit.Test;
 
 import java.util.ArrayList;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class PhoneDictionaryTest {
     @Test
@@ -15,20 +13,7 @@ public class PhoneDictionaryTest {
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        phones.add(new Person("1", "2", "3", "4"));
-        ArrayList<Person> persons = phones.find("etr");
+        ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
-    }
-
-    @Test
-    public void whenDontFind() {
-        PhoneDictionary phones = new PhoneDictionary();
-        phones.add(
-                new Person("Petr", "Arsentev", "534872", "Bryansk")
-        );
-        phones.add(new Person("1", "2", "3", "4"));
-        ArrayList<Person> persons = phones.find("tset");
-        assertTrue(persons.isEmpty());
-
     }
 }
